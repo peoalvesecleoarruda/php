1b525e1d727e33e9955e455e4b1b34294a31118f
peoalvesecleoarruda <peoalves28@gmail.com>
2024-12-05 11:35:26 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 02 PHP (Livrando da maldição)</title>
+</head>
+<body>
+    <h1>
+<?php
+echo "Olá, mundo!\u{1F30E}";
+?>
+    </h1>
+    <p>vamos tentar nos livar da maldição</p>
+</body>
+</html>
